3a263a1d4ad28f3e817cea420fce16149719e0ab
Audrius Ausauskas <aaaudrius@gmail.com>
2022-11-24 16:42:19 +0200

    whis-1 remove commented code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import Layout from 'components/Layout/Layout';
 import { useQuery } from 'react-query';
 import { fetchStatus } from 'api/health';
-// import { MyMoviesLogo } from 'components/Icons';
-
-// import styles from './App.module.css';
 
 function App(): JSX.Element {
   const { data: healthy } = useQuery('status', fetchStatus);
