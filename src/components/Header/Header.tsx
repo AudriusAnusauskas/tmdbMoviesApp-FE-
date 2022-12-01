be54f1d916d0b36faa4d5ffd0454f8066ad78af3
Audrius Ausauskas <aaaudrius@gmail.com>
2022-12-01 22:38:44 +0200

    whis-8 create sidebar component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,8 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState, useEffect } from 'react';
 import { MyMoviesLogo } from 'components/Icons';
+import Sidebar from 'components/Sidebar/Sidebar';
+import HamburgerButton from 'components/HamburgerButton/HamburgerButton';
+import useMediaQuery from 'components/Hooks/useMediaQuery';
 
 import styles from './Header.module.css';
 
@@ -8,9 +11,35 @@ interface Props {
 }
 
 const Header: React.FunctionComponent<Props> = ({ ...props }: Props) => {
+  const [sidebarShow, setSidebarShow] = useState(false);
+  const { matches } = useMediaQuery({ matchQuery: '(max-width: 768px)' });
+
+  useEffect(() => {
+    if (matches) {
+      setSidebarShow(false);
+    }
+  }, [matches]);
+
+  const handleButtonClick = () => {
+    setSidebarShow((sidebarShow) => !sidebarShow);
+  };
+
+  const closeSidebar = () => {
+    setSidebarShow(false);
+  };
+
   return (
     <header className={styles.header} {...props}>
       <MyMoviesLogo className={styles.icon} />
+      <>
+        {matches && <HamburgerButton isActive={sidebarShow} onClick={handleButtonClick} />}
+        {sidebarShow && (
+          <Sidebar onBackdropClick={closeSidebar}>
+            <div>Hello</div>
+            <div>Goodbye</div>
+          </Sidebar>
+        )}
+      </>
     </header>
   );
 };
