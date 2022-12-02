3937f2fd71b8f3f48526f19e08541917f6bb405d
Audrius Ausauskas <aaaudrius@gmail.com>
2022-12-02 22:00:28 +0200

    whis-8 implement changes for pr7

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,29 +11,29 @@ interface Props {
 }
 
 const Header: React.FunctionComponent<Props> = ({ ...props }: Props) => {
-  const [sidebarShow, setSidebarShow] = useState(false);
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
   const { matches } = useMediaQuery({ matchQuery: '(max-width: 768px)' });
 
   useEffect(() => {
     if (matches) {
-      setSidebarShow(false);
+      setIsSidebarVisible(false);
     }
   }, [matches]);
 
   const handleButtonClick = () => {
-    setSidebarShow((sidebarShow) => !sidebarShow);
+    setIsSidebarVisible((isSidebarVisible) => !isSidebarVisible);
   };
 
   const closeSidebar = () => {
-    setSidebarShow(false);
+    setIsSidebarVisible(false);
   };
 
   return (
     <header className={styles.header} {...props}>
       <MyMoviesLogo className={styles.icon} />
       <>
-        {matches && <HamburgerButton isActive={sidebarShow} onClick={handleButtonClick} />}
-        {sidebarShow && (
+        {matches && <HamburgerButton isActive={isSidebarVisible} onClick={handleButtonClick} />}
+        {isSidebarVisible && (
           <Sidebar onBackdropClick={closeSidebar}>
             <div>Hello</div>
             <div>Goodbye</div>
