57407ba729743b8f06e6dd1effb9d117662247c9
Audrius Ausauskas <aaaudrius@gmail.com>
2023-01-01 22:14:54 +0200

    correct minor issues with code order

--- src/api/movies/movieDetails.ts
+++
@@ -1,8 +0,0 @@
-import { get } from 'api/shared/methods';
-
-import { MovieDetails } from './types';
-
-export async function getMovie(id: string): Promise<MovieDetails> {
-  const { data } = await get<MovieDetails>(`movie/${id}`);
-  return data;
-}
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,5 @@
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
-// import MoviesListContainer from 'containers/MoviesListContainer/MoviesListContainer';
-// import MovieInfoContainer from 'containers/MovieInfoContainer/MovieInfoContainer';
 import { ReactNode } from 'react';
 
 import styles from './Layout.module.css';
