904a865120b6a41c731fcc98e3627e64e7376747
Audrius Ausauskas <aaaudrius@gmail.com>
2023-03-04 13:22:24 +0200

    change variable naming

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -9,17 +9,17 @@ type ModalProps = {
 };
 
 const Modal: React.FC<ModalProps> = (props: ModalProps) => {
-  const [signUpSignIn, setSignUpSignIn] = useState(true);
+  const [shouldDisplaySignUpForm, setShouldDisplaySignUpForm] = useState(true);
   const [signUpValues, setSignUpValues] = useState({ name: '', email: '', password: '' });
 
   const toggleForms = (values: any) => {
-    setSignUpSignIn(!signUpSignIn);
+    setShouldDisplaySignUpForm(!shouldDisplaySignUpForm);
     setSignUpValues(values);
   };
 
   return (
     <div className={styles.backdrop}>
-      {signUpSignIn ? (
+      {shouldDisplaySignUpForm ? (
         <SignUpForm handleClose={props.handleClose} toggleForms={toggleForms} />
       ) : (
         <SignInForm handleClose={props.handleClose} signUpValues={signUpValues} toggleForms={toggleForms} />
